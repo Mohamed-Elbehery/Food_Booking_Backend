d69d31af21fd49cf633ccb373a3120df0423dcec
Mohamed El-Sharqawy <dev.elbehery@gmail.com>
2024-07-11 07:49:40 +0300

    auth bug fixed

--- controllers/auth.controllers.ts
+++ controllers/auth.controllers.ts
@@ -15,44 +15,52 @@ export class AuthControllers {
   public constructor() {}
 
   public static async validateToken(req: Request, res: Response) {
-    const { _id } = req.query;
-
     const token = req.header("Authorization")?.replace("Bearer ", "");
 
     if (!token) {
-      return res
-        .status(401)
-        .json({ message: "Authentication token is missing" });
+      throw new Error("Authentication token is missing");
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY!) as IDecoded;
-
     const user = await User.findById(decoded._id);
+
     if (!user) {
-      return res.status(401).json({ message: "User not found" });
+      throw new Error("User not found");
     }
 
     if (user.role !== "admin") {
-      return res
-        .status(403)
-        .json({ message: "You don't have permissions to make this request" });
+      throw new Error("You don't have permissions to make this request");
     }
 
-    if (user._id.toString() == _id)
-      return res
-        .status(400)
-        .json({ message: "Error, you can't change your role." });
+    return user._id.toString();
   }
 
   public async getAllUsers(req: Request, res: Response) {
     try {
-      AuthControllers.validateToken(req, res);
+      await AuthControllers.validateToken(req, res);
 
       const allUsers = await User.find({});
-      res.status(200).json({ data: allUsers });
+      return res.status(200).json({ data: allUsers });
     } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: "Server error" });
+      const msg = (err as Error).message;
+
+      const error = msg.includes("Authentication token is missing")
+        ? "Authentication token is missing"
+        : msg.includes("User not found")
+        ? "User not found"
+        : msg.includes("You don't have permissions to make this request")
+        ? "You don't have permissions to make this request"
+        : "Server Error";
+
+      const STATUS = msg.includes("Authentication token is missing")
+        ? 401
+        : msg.includes("User not found")
+        ? 404
+        : msg.includes("You don't have permissions to make this request")
+        ? 403
+        : 500;
+
+      return res.status(STATUS).json({ message: error });
     }
   }
 
@@ -60,13 +68,30 @@ export class AuthControllers {
     try {
       const { _id } = req.query;
 
-      AuthControllers.validateToken(req, res);
+      await AuthControllers.validateToken(req, res);
 
       const user = await User.findById(_id);
-      res.status(200).json(user);
+      return res.status(200).json(user);
     } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: "Server error" });
+      const msg = (err as Error).message;
+
+      const error = msg.includes("Authentication token is missing")
+        ? "Authentication token is missing"
+        : msg.includes("User not found")
+        ? "User not found"
+        : msg.includes("You don't have permissions to make this request")
+        ? "You don't have permissions to make this request"
+        : "Server Error";
+
+      const STATUS = msg.includes("Authentication token is missing")
+        ? 401
+        : msg.includes("User not found")
+        ? 404
+        : msg.includes("You don't have permissions to make this request")
+        ? 403
+        : 500;
+
+      return res.status(STATUS).json({ message: error });
     }
   }
 
@@ -90,13 +115,31 @@ export class AuthControllers {
 
       const token = createToken(user._id);
 
-      res.status(201).json({ date: user, token });
+      return res.status(201).json({ date: user, token });
     } catch (err) {
-      const errorMessage = (err as Error).message;
-      console.error(errorMessage);
-      if (errorMessage.includes("E11000"))
-        res.status(400).json({ message: "Email is alreaady taken!" });
-      res.status(500).json({ error: "Server error" });
+      const msg = (err as Error).message;
+
+      if (msg.includes("E11000")) {
+        return res.status(400).json({ message: "Email is alreaady taken!" });
+      }
+
+      const error = msg.includes("Authentication token is missing")
+        ? "Authentication token is missing"
+        : msg.includes("User not found")
+        ? "User not found"
+        : msg.includes("You don't have permissions to make this request")
+        ? "You don't have permissions to make this request"
+        : "Server Error";
+
+      const STATUS = msg.includes("Authentication token is missing")
+        ? 401
+        : msg.includes("User not found")
+        ? 404
+        : msg.includes("You don't have permissions to make this request")
+        ? 403
+        : 500;
+
+      return res.status(STATUS).json({ message: error });
     }
   }
 
@@ -124,13 +167,31 @@ export class AuthControllers {
 
       const token = createToken(user._id);
 
-      res.status(201).json({ date: user, token });
+      return res.status(201).json({ date: user, token });
     } catch (err) {
-      const errorMessage = (err as Error).message;
-      console.error(errorMessage);
-      if (errorMessage.includes("E11000"))
-        res.status(400).json({ message: "Email is alreaady taken!" });
-      res.status(500).json({ error: "Server error" });
+      const msg = (err as Error).message;
+
+      if (msg.includes("E11000")) {
+        return res.status(400).json({ message: "Email is alreaady taken!" });
+      }
+
+      const error = msg.includes("Authentication token is missing")
+        ? "Authentication token is missing"
+        : msg.includes("User not found")
+        ? "User not found"
+        : msg.includes("You don't have permissions to make this request")
+        ? "You don't have permissions to make this request"
+        : "Server Error";
+
+      const STATUS = msg.includes("Authentication token is missing")
+        ? 401
+        : msg.includes("User not found")
+        ? 404
+        : msg.includes("You don't have permissions to make this request")
+        ? 403
+        : 500;
+
+      return res.status(STATUS).json({ message: error });
     }
   }
 
@@ -141,10 +202,27 @@ export class AuthControllers {
       const token = createToken(user._id);
       user.password = "";
 
-      res.json({ data: user, token });
+      return res.json({ data: user, token });
     } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: "Server error" });
+      const msg = (err as Error).message;
+
+      const error = msg.includes("Authentication token is missing")
+        ? "Authentication token is missing"
+        : msg.includes("User not found")
+        ? "User not found"
+        : msg.includes("You don't have permissions to make this request")
+        ? "You don't have permissions to make this request"
+        : "Server Error";
+
+      const STATUS = msg.includes("Authentication token is missing")
+        ? 401
+        : msg.includes("User not found")
+        ? 404
+        : msg.includes("You don't have permissions to make this request")
+        ? 403
+        : 500;
+
+      return res.status(STATUS).json({ message: error });
     }
   }
 
@@ -152,22 +230,44 @@ export class AuthControllers {
     try {
       const { _id, role } = req.query;
 
-      await AuthControllers.validateToken(req, res);
+      const adminID = await AuthControllers.validateToken(req, res);
 
       if (role != "admin" && role != "normal")
         return res
           .status(400)
           .json({ message: 'User role should be either "admin" or "normal"' });
 
+      if (adminID == _id)
+        return res
+          .status(400)
+          .json({ message: "Error, you can't change your role." });
+
       const updatedUser = await User.updateOne({ _id }, { role });
 
-      res.status(200).json({
+      return res.status(200).json({
         data: updatedUser,
         message: `User Role Changed Successfully to ${role}`,
       });
     } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: "Server error" });
+      const msg = (err as Error).message;
+
+      const error = msg.includes("Authentication token is missing")
+        ? "Authentication token is missing"
+        : msg.includes("User not found")
+        ? "User not found"
+        : msg.includes("You don't have permissions to make this request")
+        ? "You don't have permissions to make this request"
+        : "Server Error";
+
+      const STATUS = msg.includes("Authentication token is missing")
+        ? 401
+        : msg.includes("User not found")
+        ? 404
+        : msg.includes("You don't have permissions to make this request")
+        ? 403
+        : 500;
+
+      return res.status(STATUS).json({ message: error });
     }
   }
 }
