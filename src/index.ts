114085cd57b47ba47018107afa65cd5b4e33baf0
moaazjr <moaaz0free@gmail.com>
2024-08-19 22:57:46 +0300

    fix cors errors

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser";
-import cors from 'cors';
+import cors from "cors";
 import { dbInstance } from "../lib/db";
 import { menuRouter } from "../routes/menu.routes";
 import { bookingRouter } from "../routes/booking.routes";
@@ -13,7 +13,7 @@ dotenv.config();
 export const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors())
+app.use(cors());
 
 // parse application/json
 app.use(bodyParser.json());
